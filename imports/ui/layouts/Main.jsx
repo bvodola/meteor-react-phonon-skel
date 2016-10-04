0033d512bab103f3444116e3e4c1993457aa8f9a
Brunno Vodola <bvodola@gmail.com>
2016-10-04 02:18:29 -0300

    Added .content class to {children} in Main Layout

--- imports/ui/layouts/Main.jsx
+++ imports/ui/layouts/Main.jsx
@@ -3,16 +3,19 @@ import SidePanel from './SidePanel.jsx';
 
 const Main = ({children}) => (
 	<div className="main">
-			<SidePanel />
-			<home data-page="true">
-				<header className="header-bar">
-		        <button className="btn icon icon-arrow-back pull-left"></button>
-		        <button className="btn icon icon-menu pull-right show-for-phone-only" data-side-panel-id="side-panel-example"></button>
-		        <div className="center">
-		            <h1 className="title">App</h1>
-		        </div>
-		    </header>
-			{children}
+		<SidePanel />
+		<home data-page="true">
+			<header className="header-bar">
+	        <button className="btn icon icon-arrow-back pull-left"></button>
+	        <button className="btn icon icon-menu pull-right show-for-phone-only" data-side-panel-id="side-panel-example"></button>
+	        <div className="center">
+	            <h1 className="title">App</h1>
+	        </div>
+	    </header>
+
+			<div className="content">
+				{children}
+			</div>
 		</home>
 	</div>
 );
